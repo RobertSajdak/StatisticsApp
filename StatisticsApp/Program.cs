d76e3c3a3a22a4c671a3d6c5d1bd8b8fea4b196f
RobertSajdak <robert.sajdak@onet.eu>
2023-02-05 19:13:06 +0100

    Klasa Employee z pętlą foreach.

--- StatisticsApp/Program.cs
+++ StatisticsApp/Program.cs
@@ -8,36 +8,7 @@ employee.AddGrade(1);
 employee.AddGrade(2);
 employee.AddGrade(3);
 
-/*var statistics = employee.GetStatistics();
+var statistics = employee.GetStatistics();
 Console.WriteLine($"min: {statistics.Min}");
 Console.WriteLine($"max: {statistics.Max}");
 Console.WriteLine($"average: {statistics.Average:N2}");
-*/
-
-Console.WriteLine();
-var statistics1 = employee.GetStatisticsWithForEach();
-Console.WriteLine("Wyniki dla pętli 'foreach':");
-Console.WriteLine($"Min: {statistics1.Min}");
-Console.WriteLine($"Max: {statistics1.Max}");
-Console.WriteLine($"Average: {statistics1.Average:N2}");
-
-Console.WriteLine();
-var statistics2 = employee.GetStatisticsWithFor();
-Console.WriteLine("Wyniki dla pętli 'for':");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-
-Console.WriteLine();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-Console.WriteLine("Wyniki dla pętli 'do-while':");
-Console.WriteLine($"Min: {statistics3.Min}");
-Console.WriteLine($"Max: {statistics3.Max}");
-Console.WriteLine($"Average: {statistics3.Average:N2}");
-
-Console.WriteLine();
-var statistics4 = employee.GetStatisticsWithWhile();
-Console.WriteLine("Wyniki dla pętli 'while':");
-Console.WriteLine($"Min: {statistics4.Min}");
-Console.WriteLine($"Max: {statistics4.Max}");
-Console.WriteLine($"Average: {statistics4.Average:N2}");
